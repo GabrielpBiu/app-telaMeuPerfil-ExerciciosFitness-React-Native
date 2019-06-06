5ad90c4f9a804d9eff1369bc0da66cc171a9fdcf
GabrielpBiu <gabrielpbiu@gmail.com>
2019-06-06 11:27:05 -0300

    Imagens

---
+++ images/img.js
@@ -0,0 +1,8 @@
+exports.ic_yoga = require('../images/ic_yoga.png');
+exports.ic_upper_body = require('../images/ic_upper_body.png');
+exports.ic_lower_body = require('../images/ic_lower_body.png'); 
+exports.ic_dance = require('../images/ic_dance.png'); 
+exports.running = require('../images/running.png'); 
+exports.cycling = require('../images/cycling.png'); 
+exports.bodybuilding = require('../images/gym.png'); 
+exports.yoga = require('../images/yoga.png');
