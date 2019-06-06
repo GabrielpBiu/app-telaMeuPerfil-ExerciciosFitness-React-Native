c1923162662b6df2ca1dedc0a8603d7ad7decb72
GabrielpBiu <gabrielpbiu@gmail.com>
2019-06-06 11:13:22 -0300

    Estrutura

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './components/MeuPerfil';
+import _ from 'lodash';
+
+
+export default class App extends React.Component {
+
+  render() {
+    
+    return <Home />;
+  }
+}
